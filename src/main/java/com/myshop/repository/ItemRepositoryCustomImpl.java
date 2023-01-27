800c0ea80260d63af52839bfae4150d8f64a3fee
chdk0842 <chdk0842@naver.com>
2023-01-27 10:09:33 +0900

    up

--- src/main/java/com/myshop/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/com/myshop/repository/ItemRepositoryCustomImpl.java
@@ -12,10 +12,15 @@ import org.springframework.data.domain.Pageable;
 
 import com.myshop.constant.ItemSellStatus;
 import com.myshop.dto.ItemSearchDto;
+import com.myshop.dto.MainItemDto;
+import com.myshop.dto.QMainItemDto;
 import com.myshop.entity.Item;
 import com.myshop.entity.QItem;
+import com.myshop.entity.QItemImg;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Wildcard;
+
 import org.thymeleaf.util.StringUtils;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -70,10 +75,54 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
                 .limit(pageable.getPageSize()) //한 번에 가지고 올 최대 개수
                 .fetch();
 				
-		  long total = content.size();
+		  long total = queryFactory.select(Wildcard.count).from(QItem.item)
+	                .where(regDtsAfter(itemSearchDto.getSearchDateType()),
+	                        searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
+	                        searchByLike(itemSearchDto.getSearchBy(), itemSearchDto.getSearchQuery()))
+	                .fetchOne();
+	      
+	      
+	      return new PageImpl<>(content, pageable, total);
 		  
-		  return new PageImpl<>(content, pageable, total);
 
+	}
+	
+	private BooleanExpression itemNmLike(String searchQuery) {
+		return StringUtils.isEmpty(searchQuery) ? null : QItem.item.itemNm.like("%" + searchQuery + "%");
+				
+	}
+
+	@Override
+	public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+		QItem item = QItem.item;
+		QItemImg itemImg = QItemImg.itemImg;
+		
+		List<MainItemDto> content = queryFactory.select(
+				new QMainItemDto(
+				item.id,
+				item.itemNm,
+				item.itemDetail,
+				itemImg.imgUrl,
+				item.price)
+			)
+				.from(itemImg)
+				.join(itemImg.item, item)
+				.where(itemImg.repimgYn.eq("Y"))
+				.where(itemNmLike(itemSearchDto.getSearchQuery()))
+				.orderBy(item.id.desc())
+				.offset(pageable.getOffset()) //데이터를 가져올 시작 index
+	            .limit(pageable.getPageSize()) //한 번에 가지고 올 최대 개수
+				.fetch();
+		
+		//count(*)
+		long total = queryFactory.select(Wildcard.count)
+				.from(itemImg)
+				.join(itemImg.item, item)
+				.where(itemImg.repimgYn.eq("Y"))
+				.where(itemNmLike(itemSearchDto.getSearchQuery()))
+				.fetchOne();
+		
+		return new PageImpl<>(content, pageable, total);
 	}
 
 }
